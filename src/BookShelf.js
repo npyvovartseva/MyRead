ace35c981e016bc1944c56d054ff05dcd3047972
Nataliia Pyvovartseva <raxma@ukr.net>
2020-12-12 21:52:36 +0200

    Create components: Book, BooksList, BookShelf

---
+++ src/BookShelf.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import BooksList from './BooksList';
+
+class BookShelf extends Component{
+
+    render(){
+        return(
+            <div><BookShelf /></div>
+        )
+    }
+}
+
+export default BookShelf;
---
+++ src/BooksList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Book from './Book';
+
+class BooksList extends Component{
+
+    render(){
+        return(
+            <div><Book /></div>
+        )
+    }
+}
+
+export default BooksList;
